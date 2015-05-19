68ccb1e1fded357e526224bb03e1b0e824ee919d
Siskica32 <milosgandric@gmail.com>
2015-05-19 04:46:55 +0200

    proizvod i video

---
+++ vunica/CodeIgniter-3.0.0/application/CSS/Postavljanje videa.js
@@ -0,0 +1,33 @@
+// JavaScript Document
+
+var kolicina = 1;
+var mouse = false;
+
+function preostalo(kap){
+	document.getElementById("preostalo").innerHTML = kap - document.getElementById("opisVidea").value.length;
+}
+
+function postavljanjeKolicine(operacija){
+	if(mouse) {
+		if(operacija == '+'){
+			kolicina++;
+			document.getElementById("kolicina").innerHTML = kolicina;
+			setTimeout("postavljanjeKolicine('+')",200);
+		}
+		else if(operacija == '-' && kolicina>1){
+			kolicina--;
+			document.getElementById("kolicina").innerHTML = kolicina;
+		    setTimeout("postavljanjeKolicine('-')",200);
+		}
+	}
+}
+
+function mousedown(operacija)
+{	
+  mouse = true;
+  postavljanjeKolicine(operacija);
+}
+function mouseup()
+{
+  mouse =false;
+}
