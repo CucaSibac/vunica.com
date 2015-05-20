ff24ab24fc14d31a34ebf66131ffb2242322cb52
TeodoraAleksic <at120391d@student.etf.rs>
2015-05-20 23:56:46 +0200

    Ispravka headera

--- vunica/application/views/Header.php
+++ vunica/application/views/Header.php
@@ -6,16 +6,16 @@
                   <!-- Prazna celija-->
             </td>
             <td id = "logoPolje">
-                <img src="http://localhost/Slike/Vunica/vunica.png" id = "logo" onclick="window.open('http://localhost/vunica.com/vunica/index.php/Index', '_parent')">
+                <img src="http://localhost/Slike/Vunica/vunica.png" id = "logo" onclick="window.open('http://localhost/vunica.com/vunica/index.php/IndexStrana', '_parent')">
             </td>
             <td width = "8%" class = "poljeMenija" onclick="window.open('http://localhost/vunica.com/vunica/index.php/Pocetna', '_parent')">
-                <font class = "opcijeMenija"> Pocetna </font>
+                <font class = "opcijeMenija"> Početna </font>
             </td>
             <td width = "1%" class="poljeMenija">
               <font color="#605951" face="Tahoma, Geneva, sans-serif" size="3"> | </font>
             </td>
 			<td width = "8%" class = "poljeMenija" onclick="window.open('http://localhost/vunica.com/vunica/index.php/Strikarnica', '_parent')">
-                <font class = "opcijeMenija"> Srikarnica </font>
+                <font class = "opcijeMenija"> Štrikarnica </font>
             </td>
             <td width = "1%" class = "poljeMenija">
               <font color="#605951" face="Tahoma, Geneva, sans-serif" size="3"> | </font>
