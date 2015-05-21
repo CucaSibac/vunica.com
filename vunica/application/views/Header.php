b729fac4a27610e145733d89a22c386f300ceb5a
TeodoraAleksic <at120391d@student.etf.rs>
2015-05-21 18:55:58 +0200

    Ubacivanje odjave u header - ne radi

--- vunica/application/views/Header.php
+++ vunica/application/views/Header.php
@@ -27,18 +27,29 @@
                 <!-- Prazna celija bez zadate sirine -->
             </td>
             <td id = "poljePrijava">
-              <!-- InstanceBeginEditable name="Prijava" -->
-              <font class = "opcijeMenija" style="white-space: nowrap;"> 
-                <font onclick="prikazi_prijavu()">Prijava</font>
-                /
-                <font onclick="prikazi_registraciju()">Registracija</font>
+              <font class = "opcijeMenija" style="white-space: nowrap;">
+                  <?php if($this->session->UserName != ''){ ?>
+                      <font onclick = "window.open('http://localhost/vunica.com/vunica/index.php/Profil', '_parent')">
+                           <?php echo $this->session->UserName; ?>
+                      </font>
+                      /
+                      <?php 
+                        $attributes = array('id' => 'formaOdjava');
+                        echo form_open("PopUp/logout", $attributes); 
+                      ?>
+                        <input class = "odjava" type = "submit" value = "Odjava"/>
+                      <?php echo form_close();?>
+                  <?php }else{ ?>
+                    <font onclick="prikazi_prijavu()">Prijava</font>
+                    /
+                    <font onclick="prikazi_registraciju()">Registracija</font>
+                  <?php } ?>
               </font> 
-              <!-- InstanceEndEditable -->
             </td>
             <td id = "cegerPolje">
-              <!-- InstanceBeginEditable name="Ceger" -->
-              <img src="http://localhost/Slike/Ceger/Ceger.png" id = "ceger" onclick="window.open("Ceger")">  
-              <!-- InstanceEndEditable -->
+              <?php if($this->session->UserName != ''){ ?>
+                <img src="http://localhost/Slike/Ceger/Ceger.png" id = "ceger" onclick="window.open('http://localhost/vunica.com/vunica/index.php/Ceger', '_parent')"/>  
+              <?php } ?>
             </td>
           </tr>
       </table>
