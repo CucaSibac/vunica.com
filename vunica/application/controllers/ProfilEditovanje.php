50c5c4826c13b791659aee4e5c65ba6d9591f8c0
TeodoraAleksic <at120391d@student.etf.rs>
2015-05-21 17:09:25 +0200

    Nastavak mojih muka

--- vunica/application/controllers/ProfilEditovanje.php
+++ vunica/application/controllers/ProfilEditovanje.php
@@ -10,8 +10,24 @@ class ProfilEditovanje extends CI_Controller {
         $this->podaci['korisnici'] = $this->Korisnik->ucitajKorisnika();
         
         $this->kopija = $this->podaci;
+
+        if(($this->session->userdata('UserName')!="")){
+            $set_data = $this->session->all_userdata();
+        }
+        else{
+            $set_data = array(
+                'IDKorisnik'   =>'',
+                'UserName'  =>'',      
+                'ImePrezime' =>'',
+                'Status' => '',
+                'Slika' => 'http://localhost/Slike/Profilna/Nedefinisano.jpg',
+                'Email'     => '',
+                'Opis'     => '',
+                'logged_in' => FALSE,
+            );
+        }
         
-        $this->load->view('ProfilEditovanje', $this->podaci);
+        $this->load->view('ProfilEditovanje', $this->podaci, $set_data);
     }
     
     function promeni(){
