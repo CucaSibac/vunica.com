797e90089d6ae70e4c7fe032551fb9e0cdad950f
MjauMjauMackica <ivanamusic10@gmail.com>
2015-05-22 19:02:46 +0200

    izmene

--- vunica/application/models/Proizvod_model.php
+++ vunica/application/models/Proizvod_model.php
@@ -1,8 +1,35 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class Proizvod_model extends CI_Model{
+
+    function getAllForProizvod($vredn) {
+        $nizp = null;
+        $this->db->select('*');
+        $this->db->from('proizvod');
+        $this->db->where('IDProizvod', $vredn);
+        
+        $upit = $this->db->get();
+        if($upit->num_rows() > 0) {
+            foreach ($upit->result() as $red){
+                $nizp[] = $red;
+            }
+        }
+        return $nizp;
+    }
+    
+    function get_messagesp($vredn, $offset = 0 ){
+        $this->db->where('IDProizvod', $vredn);
+        $this->db->order_by('IDKomentar', 'desc');
+	$query = $this->db->get('komentarproizvod', 3, $offset);
+	return $query->result();
+    }
+    
+    
+    function num_messagesp($vredn) {
+        $this->db->where('IDProizvod', $vredn);
+        $query = $this->db->count_all_results('komentarproizvod');
+        echo $query;
+	return $query;
+    }
+}
 
