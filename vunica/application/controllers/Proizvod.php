3203fc2cd315b3f4a222325accb548c7a6508696
MjauMjauMackica <ivanamusic10@gmail.com>
2015-05-22 19:02:38 +0200

    izmene

--- vunica/application/controllers/Proizvod.php
+++ vunica/application/controllers/Proizvod.php
@@ -2,9 +2,18 @@
 
 class Proizvod extends CI_Controller {
     
-    function index(){
-        //$this->load->model('Proizvod_model');
-        $this->load->view('Proizvod');
+    public function index($vredn) {
+        $this->load->model('Proizvod_model');
+        $nizp['podacip'] = $this->Proizvod_model->getAllForProizvod($vredn);
+        $nizp['num_messagesp'] = $this->Proizvod_model->num_messagesp($vredn);
+        $nizp['latest_messagesp'] = $this->Proizvod_model->get_messagesp($vredn);
+        $this->load->view('Proizvod', $nizp);
+    }
+
+    function get_messagesp($vredn, $offset ) {
+        $this->load->model('Video_model');
+        $nizp['latest_messagesp'] = $this->Proizvod_model->get_messagesp($vredn, $offset);
+        $this->load->view('get_messagesp', $nizp);
     }
 }
 
