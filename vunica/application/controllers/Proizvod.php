3e204e85e824440ee233721bcaee95c3291dcb9f
MjauMjauMackica <ivanamusic10@gmail.com>
2015-05-22 19:20:46 +0200

    izmene

--- vunica/application/controllers/Proizvod.php
+++ vunica/application/controllers/Proizvod.php
@@ -1,10 +1,17 @@
 <?php
 
+/*
+ * potrebno je pored &vredn koja predstavlja IDProizvod staviti i $vrednk koji predstavlja IDKorisnik
+ * ovo se stavlja kada je korisnik ulogovan
+ * ne znam kako ovo da citam iz baze
+ */
+
 class Proizvod extends CI_Controller {
     
     public function index($vredn) {
         $this->load->model('Proizvod_model');
         $nizp['podacip'] = $this->Proizvod_model->getAllForProizvod($vredn);
+        $nizp['ucegeru'] = $this->Proizvod_model->dohvatiKolicinu($vredn);
         $nizp['num_messagesp'] = $this->Proizvod_model->num_messagesp($vredn);
         $nizp['latest_messagesp'] = $this->Proizvod_model->get_messagesp($vredn);
         $this->load->view('Proizvod', $nizp);
