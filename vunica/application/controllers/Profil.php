cb15acb8471fc6ca53d947ac198048ff789a1f5f
MjauMjauMackica <ivanamusic10@gmail.com>
2015-05-23 13:18:25 +0200

    izmene

--- vunica/application/controllers/Profil.php
+++ vunica/application/controllers/Profil.php
@@ -8,6 +8,18 @@ class Profil extends CI_Controller{
     
     public function index(){
         $this->load->view('Profil');
+        //$nizk['kolicinap'] = $this->Profil_model->broj_proizvod();
+       // $nizk['poslednjip'] = $this->Profil_model->uzmi_proizvod();
+        //$nizk['kolicinav'] = $this->Profil_model->broj_video();
+        //$nizk['poslednjiv'] = $this->Profil_model->uzmi_video();
+       // $this->load->view('Profil', $nizk);
     }
+
+    function broj_proizvod($offset) {
+        $this->load->model('Profil_model');
+        $nizk['poslednjip'] = $this->Profil_model->uzmi_proizvod($offset);
+        $this->load->view('get_proizvod', $nizk);
+    }
+    
 }
 
