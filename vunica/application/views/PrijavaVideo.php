2609ab3cda5d9399e9aeb664152ce63dd4667418
Tijana Trifunovic <tt120311d@student.etf.rs>
2015-05-29 02:06:28 +0200

    ttizmena

--- vunica/application/views/PrijavaVideo.php
+++ vunica/application/views/PrijavaVideo.php
@@ -1,20 +1,15 @@
-<link rel="stylesheet" media="screen and (max-device-width: 1400px)" type="text/css" href="../CSS/Pop up (1366x768).css">
-<link rel="stylesheet" media="screen and (min-device-width: 1401px)" type="text/css" href="../CSS/Pop up (1920x1080).css">
-
-<!--Prozor koji potamni-->
-    <div id = "prazan" onclick="sakrij_video()">
-    </div>
+<?php include("Upozorenje.php");?>
 
 <!--Prozor za prijavu videa-->
  <div id = "prijavaVidea">
       <div class = "naslovPrijavaVidea"> Prijava videa </div>
 
       <img src="http://localhost/Slike/Pop-up/Putaca 3.png" class="slikaPopUp" onclick="sakrij_video()"/>
-
-      <img src="http://localhost/Slike/Pop-up/Putaca 3.png" class="slikaPopUp" onclick="sakrij_video()"/>
-
-      <form style="height:100%;" name="prijavaVidea" action="prijavaVidea.php" method = "post">
-        <table class="tabelaPrijavaVidea">
+      <?php 
+            $attributes = array('style' => 'height: 100%;', 'id' => 'formaPrijavaVideo');
+            echo form_open("Prijava/video", $attributes); 
+          ?>
+     <table class="tabelaPrijavaVidea">
           <tr>
             <td class="praznaCelijaPopUp">&nbsp;</td>
             <td colspan="2">
@@ -28,14 +23,16 @@
           </tr>    
           <tr>
             <td class="praznaCelijaPopUp">&nbsp;</td>
-            <td colspan="2"><input type="text" value="" name="tekst" id="vid" placeholder="Prijavljujem video zbog..." class="inputPopUpPrijava" /> </td>
+            <td colspan="2">
+                 <textarea  value="<?php echo set_value('Tekst'); ?>" name="Tekst" maxlength="400" style="width:80%; height:120px; resize:none;text-align: justify;" placeholder="Unesite razlog prijave" class="tekstPoljeKomentar" id="limitedtextarea" onKeyDown="limitText(400);" onKeyUp="limitText(400);"></textarea>
+                 <font class="preostalokaraktera">Preostalo karaktera: <font id="ostatak">400</font> </td>
             <td class="praznaCelijaPopUp">&nbsp;</td>
           </tr>
           <tr>
             <td class="praznaCelijaPopUp">&nbsp;</td>
-            <td class="celijaDugmePopUp"> <div class="dugmePopUp" id="obrisiprijavip" onclick="prijavi_video()"> Da</div> <br /></td>
+            <td class="celijaDugmePopUp"> <div class="dugmePopUp" onclick="document.forms['formaPrijavaVideo'].submit()"> </div> <br /></td>
 
-            <td class="celijaDugmePopUp" align="right"><div class="dugmePopUp" onclick="sakrij_video()"> Ne</div><br /></td>
+            <td class="celijaDugmePopUp" align="right"><div class="dugmePopUp" onclick="sakrij_sve()"> Odustani</div><br /></td>
 
             <td class="praznaCelijaPopUp">&nbsp;</td>
           </tr> 
