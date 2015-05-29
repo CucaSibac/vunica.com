d5c29e8cec5e44c74515416e8cb5f54efba0bd28
TeodoraAleksic <at120391d@student.etf.rs>
2015-05-29 17:37:04 +0200

    Ceger u headeru - Ups

--- vunica/application/views/Header.php
+++ vunica/application/views/Header.php
@@ -64,7 +64,7 @@
                             if(count($niz) > 0){     
                     ?>
                         <font id = "cegerBroj"> <?php echo count($niz); ?> </font>
-                    <?php // }} ?>
+                    <?php }} ?>
                   <?php } ?>
                 </div>
             </td>
