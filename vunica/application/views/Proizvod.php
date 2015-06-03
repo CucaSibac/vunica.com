4b076bf58c3d08202cd149deff6f5c7f70046ac6
Tijana Trifunovic <tt120311d@student.etf.rs>
2015-06-03 19:17:09 +0200

    ttizmena

--- vunica/application/views/Proizvod.php
+++ vunica/application/views/Proizvod.php
@@ -1,4 +1,4 @@
-<!-- Tijana Trifunovic 311/12-->
+<!-- Tijana Trifunovic 311/12 i Ivana Lisanin 531/12 -->
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml"><!-- InstanceBegin template="/Templates/Template.dwt" codeOutsideHTMLIsLocked="false" -->
     <head>
@@ -366,8 +366,7 @@ $usersession = $this->session->userdata('UserName');
                                     <?php if ($this->session->UserName != '') { ?>//ako je korisnik prijavljen
                                         </script>
 
-                                        <a  class = "dugme" onclick="document.forms['formaStaviUCeger'].submit();
-                                                                        " > Stavi u ceger </a> 
+                                   <a  class = "dugme" onclick="document.forms['formaStaviUCeger'].submit();" style="cursor: pointer" > Stavi u ceger </a> 
 
                                         <script>
 
