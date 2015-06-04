d46c2a2ed99a31c4a5ef110f8f66c2538c87c169
TeodoraAleksic <at120391d@student.etf.rs>
2015-06-04 22:36:22 +0200

    PostavljanjeVideaTest

--- vunica/application/controllers/Testovi/PostavljanjeVideaTest.php
+++ vunica/application/controllers/Testovi/PostavljanjeVideaTest.php
@@ -15,7 +15,7 @@ class PostavljanjeVideaTest extends CI_Controller {
     
     // (1) Funkcija koja testira
     public function adresa_Test(){
-        $this->unit->run($this->adresa("D:/home/site/wwwroot/vunica/application/Video/Slika.jpg"), 'http://vunica.azurewebsites.net/vunica/application/Video/Slika.jpg', 'Testiranje putanje slike');
+        $this->unit->run($this->adresa("D:/home/site/wwwroot/vunica/application/Video/Video.mp4"), 'http://vunica.azurewebsites.net/vunica/application/Video/Video.mp4', 'Testiranje putanje slike');
         echo $this->unit->report();
     }
     
@@ -24,7 +24,7 @@ class PostavljanjeVideaTest extends CI_Controller {
         $niz = explode('/', $url);
         $duzina = sizeof($niz);
         $novi_url = "";
-        for ($i = $duzina - 1; $i < $duzina; $i++) {
+        for ($i = $duzina - 2; $i < $duzina; $i++) {
             $novi_url .= $niz[$i];
             if ($i != $duzina - 1)
                 $novi_url .= '/';
