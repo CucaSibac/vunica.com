4bcc38be232ed3ffbde6cda466fd84521049bfa4
Siskica32 <milosgandric@gmail.com>
2015-06-04 20:53:00 +0200

    sitne ispravke

--- vunica/application/controllers/PostavljanjeProizvoda.php
+++ vunica/application/controllers/PostavljanjeProizvoda.php
@@ -78,10 +78,11 @@ class PostavljanjeProizvoda extends CI_Controller {
             $data = array('slika' => '');
         //    $this->load->view('PostavljanjeProizvoda', $data);
         } else {              
-            $url = $this->adresa($this->upload->data('full_path'));
+            echo $this->upload->data('full_path');
+            //  $url = $this->adresa($this->upload->data('full_path'));
             $data = array('slika' => $url);
             $this->PostavljanjeProizvoda_model->postavi_sliku($url); 
-            echo $url;
+       //     echo $url;
         //    $this->load->view('PostavljanjeProizvoda', $data);
         }
     }
