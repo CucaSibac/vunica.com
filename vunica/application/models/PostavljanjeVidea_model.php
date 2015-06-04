da48dc28784859284379340d5065a4b8376fa538
Siskica32 <milosgandric@gmail.com>
2015-06-04 23:25:05 +0200

    sitne ispravke

--- vunica/application/models/PostavljanjeVidea_model.php
+++ vunica/application/models/PostavljanjeVidea_model.php
@@ -22,7 +22,11 @@ class PostavljanjeVidea_model extends CI_Model {
             'Video' => $this->session->userdata('proVideo'),                   
             'Kategorija' => $this->input->post('vidKat'),
             'Tezina' => $this->input->post('vidTez')
-        );      
+        );  
+        if($this->input->post('vidKat') == "Garderoba") $data['Slika'] = 'http://vunica.azurewebsites.net/vunica/application/Slike/Strikarnica/Garderoba.jpg';
+        if($this->input->post('vidKat') == "Dodaci") $data['Slika'] = 'http://vunica.azurewebsites.net/vunica/application/Slike/Strikarnica/Dodaci.jpg';
+        if($this->input->post('vidKat') == "Igracke") $data['Slika'] = 'http://vunica.azurewebsites.net/vunica/application/Slike/Strikarnica/Igracke.jpg';
+        if($this->input->post('vidKat') == "Ostalo") $data['Slika'] = 'http://vunica.azurewebsites.net/vunica/application/Slike/Strikarnica/Ostalo.jpg';
         $this->db->insert('video', $data);            
     }
     
