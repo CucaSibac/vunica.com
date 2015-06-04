6a3d8c992005bd20ec70d8ce6e4b22202f7e8cc4
Siskica32 <milosgandric@gmail.com>
2015-06-05 01:26:26 +0200

    sitne ispravke

--- vunica/application/controllers/Testovi/KupovinaProizvodaTest.php
+++ vunica/application/controllers/Testovi/KupovinaProizvodaTest.php
@@ -1,7 +1,7 @@
 <?php
 
 
-class PostavljanjeVideaTest extends CI_Controller {
+class KupovinaProizvodaTest extends CI_Controller {
     
     public function __construct() {
         parent::__construct();
