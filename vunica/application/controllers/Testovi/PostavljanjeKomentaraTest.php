886fda4fbfe9da40c312c84bc74489a68afb999c
TeodoraAleksic <at120391d@student.etf.rs>
2015-06-05 00:58:29 +0200

    Testovi nastavak

--- vunica/application/controllers/Testovi/PostavljanjeKomentaraTest.php
+++ vunica/application/controllers/Testovi/PostavljanjeKomentaraTest.php
@@ -10,5 +10,26 @@ class PostavljanjeKomentaraTest {
     
     public function index() {
         $this->load->library('unit_test');
+        $this->komentar_Test();
+    }
+    
+    // (1) Funkcija koja testira
+    public function komentar_Test(){
+        $this->unit->run($this->komentar('Ovo je test komentar1'), TRUE, 'Testiranje postavljanja komentara');
+        echo $this->unit->report();
+    }
+    
+    // (1) Funkcija koja se testira
+    public function komentar($tekst) {
+        $refering_url = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
+        if ($tekst !== "") {
+            $this->load->model('Video_model');
+            $this->Video_model->novikomentar();
+            $this->session->unset_userdata('tekstGreska');
+            return TRUE;
+        } else {
+            $this->session->set_userdata('tekstGreska', 'true');
+            return FALSE;
+        }
     }
 }
