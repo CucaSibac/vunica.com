8ed1e4eb6c61b4c1e1a69e223a95316655d0fd6c
Siskica32 <milosgandric@gmail.com>
2015-06-05 00:32:57 +0200

    sitne ispravke

--- vunica/application/models/Test_model.php
+++ vunica/application/models/Test_model.php
@@ -16,4 +16,41 @@ class Test_model extends CI_Model {
             return false;
         }               
          
+        function prijaviVideo() {
+        if ($this->input->post('Tekst') !== "") {
+            $data = array(
+                'UserName' => ($this->session->UserName),
+                'IDVideo' => ($this->session->videoId),
+                'Komentar' => ("Sadrzaj je neprikladan"),
+                'Datum' => (date("Y.m.d"))
+            );
+        } else {
+            $data = array(
+                'UserName' => ($this->session->UserName),
+                'IDVideo' => ($this->session->videoId),
+                'Komentar' => ("Sadrzaj je neprikladan"),
+                'Datum' => (date("Y.m.d"))
+            );
+        }
+        $this->db->insert('prijavavideo', $data);
+    }
+    
+    function prijaviProizvod() {
+        if ($this->input->post('Tekst') !== "") {
+            $data = array(
+                'UserName' => ($this->session->UserName),
+                'IDProizvod' => ($this->session->proizvodId),
+                'Komentar' => ("Sadrzaj je neprikladan"),
+                'Datum' => (date("Y.m.d"))
+            );
+        } else {
+            $data = array(
+                'UserName' => ($this->session->UserName),
+                'IDProizvod' => ($this->session->proizvodId),
+                'Komentar' => ("Sadrzaj je neprikladan"),
+                'Datum' => (date("Y.m.d"))
+            );
+        }
+        $this->db->insert('prijavaproizvod', $data);
+    }
 }
