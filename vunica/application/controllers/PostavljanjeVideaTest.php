99d156c9f8b6f6207e89ad896d92ece5c008ab26
TeodoraAleksic <at120391d@student.etf.rs>
2015-06-04 20:39:28 +0200

    Test proba 3

--- vunica/application/controllers/PostavljanjeVideaTest.php
+++ vunica/application/controllers/PostavljanjeVideaTest.php
@@ -3,6 +3,14 @@
 
 class PostavljanjeVideaTest extends PostavljanjeVidea {
     
+    public function __construct() {
+        parent::__construct();
+    }
+    
+    public function index() {
+        $this->adresa_Test();
+    }
+    
     public function adresa_Test(){
         $this->unit->run($this->adresa("C:\Users\IcySch\Pictures\Bzvz\auto.jpg"), 'http://localhost/vunica/index.php/Slike/auto.jpg', 'Testiranje putanje slike');
         echo $this->unit->report();
