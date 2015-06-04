852bab16ab4fd08e8b20147e3450e45cdb93f8af
Siskica32 <milosgandric@gmail.com>
2015-06-05 01:22:13 +0200

    sitne ispravke

--- vunica/application/controllers/Testovi/KupovinaProizvodaTest.php
+++ vunica/application/controllers/Testovi/KupovinaProizvodaTest.php
@@ -11,14 +11,13 @@ class PostavljanjeVideaTest extends CI_Controller {
         $this->load->library('unit_test');
         $this->adresa_Test();
     }
-    
-    // (1) Funkcija koja testira
-    public function adresa_Test(){
-        $this->unit->run($this->adresa("C:/Users/IcySch/Pictures/Bzvz/auto.jpg"), 'http://vunica.azurewebsites.net/vunica/index.php/application/Video/auto.jpg', 'Testiranje putanje slike');
+      
+    public function test1(){
+        $this->StaviUCeger();
+        $this->unit->run($this->izbaciProizvod(10), true, 'test kupovine');
         echo $this->unit->report();
     }
     
-    // (1) Funkcija koja se testira
     protected function adresa($url) {
         $niz = explode('/', $url);
         $duzina = sizeof($niz);
@@ -29,6 +28,50 @@ class PostavljanjeVideaTest extends CI_Controller {
                 $novi_url .= '/';
         }
         return 'http://localhost/' . $novi_url;
+    }
+    
+    function StaviUCeger(){
+        $IDProizvod = 10;
+        $NazivProizvoda = 'MIS';
+        $Cena = 25;
+        $KolicinaForma = 1;
+        $data = array ($IDProizvod, $NazivProizvoda, $Cena, $KolicinaForma );
+        /*    'IDProizvod' => ($this->input->post('IDProizvod')),
+            'NazivProizvoda' => ($this->input->post('NazivProizvoda'))
+            'Cena' => ($this->input->post('Cena')),
+            'KolicinaForma' => ($this->input->post('KolicinaForma'))*/
+    
+       
+        $nizProizvoda = '';
+        
+        $duzina = count($nizProizvoda);
+        if (!$this->session->Proizvodi) {
+            $duzina = 0;
+        }
+        $nizProizvoda[$duzina] = $data;
+        $this->session->set_userdata('Proizvodi', $nizProizvoda);
+    }
+    
+    public function izbaciProizvod($id){
+        $niz = $this->session->Proizvodi;
+        
+        $indeks = $this->nadjiIndeks($niz, $id);
+        
+        unset($niz[$indeks]);     
+        $this->session->Proizvodi = array_values($niz);
+        
+        $this->resetujGreske();
+        $this->load->view('Ceger', $this->greske);
+        return true;
+    }
+    
+    protected function nadjiIndeks($niz, $id){
+        $i = 0;
+        for(;$i < count($niz);$i++){
+            if($niz[$i][0] == $id){
+               return $i;
+            }
+        }
     }
 
 
