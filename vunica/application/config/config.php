660766c70a1b26c35005d2965487e7da876aba3f
TeodoraAleksic <at120391d@student.etf.rs>
2015-06-04 17:18:58 +0200

    Namestanje pocetne strane

--- vunica/application/config/config.php
+++ vunica/application/config/config.php
@@ -29,7 +29,7 @@ $config['base_url'] ='http://vunica.azurewebsites.net/vunica/'; //'http://localh
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = 'index.php/IndexStrana';
 
 /*
 |--------------------------------------------------------------------------
