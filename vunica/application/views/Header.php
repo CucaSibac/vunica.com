dfee0fed765edcb838bd0cb6f1abbec49e10262a
Siskica32 <milosgandric@gmail.com>
2015-06-15 00:13:10 +0200

    sitne ispravke

--- vunica/application/views/Header.php
+++ vunica/application/views/Header.php
@@ -42,7 +42,7 @@
             <td id = "poljePrijava" style="width: 5%; text-align: left; padding-right: 3%;">
                 
                     <!--<input class = "odjava" type = "submit" value = "Odjava"/>-->
-                <font class = "opcijeMenija" onclick="window.open('http://vunica.azurewebsites.net/vunica/index.php/PopUp/logout/')"> Odjava </font>
+                <font class = "opcijeMenija" onclick="window.open('http://vunica.azurewebsites.net/vunica/index.php/PopUp/logout/','_parent')"> Odjava </font>
                 
             </td>
             <?php }else{ ?>
